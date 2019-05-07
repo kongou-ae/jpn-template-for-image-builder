caffe80aa9692d047347eb9022c5f8a80adc9720
kongou-ae <y.matsumoto.ae@gmail.com>
2019-05-08 01:31:48 +0900

    Create ui.ps1

---
+++ ui.ps1
@@ -0,0 +1,7 @@
+#https://qiita.com/akiyoshi-t/items/76eb446cc8f8ec360e00
+
+Set-WinUILanguageOverride -Language ja-JP
+Set-WinCultureFromLanguageListOptOut -OptOut $False
+Set-WinHomeLocation -GeoId 0x7A
+Set-WinSystemLocale -SystemLocale ja-JP
+Set-TimeZone -Id "Tokyo Standard Time"
