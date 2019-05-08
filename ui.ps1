6a02deaf35123c4b352e3ae4f2baf9da1de49ed8
kongou-ae <y.matsumoto.ae@gmail.com>
2019-05-08 12:44:19 +0900

    Update ui.ps1

--- ui.ps1
+++ ui.ps1
@@ -1,8 +1,21 @@
 #https://qiita.com/akiyoshi-t/items/76eb446cc8f8ec360e00
 
 Set-WinUILanguageOverride -Language ja-JP
+
+get-WinCultureFromLanguageListOptOut
 Set-WinCultureFromLanguageListOptOut -OptOut $False
+get-WinCultureFromLanguageListOptOut
+
+get-WinHomeLocation
 Set-WinHomeLocation -GeoId 0x7A
+get-WinHomeLocation
+
+get-WinSystemLocale
 Set-WinSystemLocale -SystemLocale ja-JP
+get-WinSystemLocale
+
+get-timezone
 Set-TimeZone -Id "Tokyo Standard Time"
+get-timezone
+
 New-Item c:\aib -itemType Directory
